c689b2b01415b0be136fa1285a3e0268ed3e67f9
michal-wojcik-fav <michal.wojcik.vm@gmail.com>
2024-03-30 18:04:22 +0100

    init

---
+++ src/main/java/fav/apiMessaging/ApiMessagingApplication.java
@@ -0,0 +1,13 @@
+package fav.apiMessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fav/apiMessaging/ApiMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package fav.apiMessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
